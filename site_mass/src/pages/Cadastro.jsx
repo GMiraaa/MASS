d272cc65141fee490c890283880d397eea62ede9
GMiraaa <guifaria.mira@gmail.com>
2026-06-09 02:22:09 -0300

    OK: Add: design básico com estruturação das rotas

--- site_mass/src/pages/Cadastro.jsx
+++ site_mass/src/pages/Cadastro.jsx
@@ -1,20 +1,30 @@
 import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
-export default function cadastro (){
-    return(
-        <div className="">
-            <h2>Email:</h2>
-            <input type="text" placeholder="Seu melhor email!"/>
+export default function cadastro() {
 
-            <h2>Senha:</h2>
-            <input type="password" />
+    const navigate = useNavigate();
 
-            <h2>Confirme sua senha:</h2>
-            <input type="password" />
+    return (
+        <div className="flex bg-gray-200 w-screen h-screen items-center justify-center">
+            <button onClick={() => navigate("/login")} className="cursor-pointer absolute top-5 left-5 rounded bg-amber-200 px-5 py-2">Voltar</button>
+            <div className="flex w-2/5 h-120 rounded-lg bg-gray-300 p-10 items-center justify-center">
+                <div className="">
+                    <h2 className="text-2xl">Email:</h2>
+                    <input type="text" placeholder="Seu melhor email!" className="bg-white rounded px-3 py-2" /><br /><br />
 
-            <br />
+                    <h2 className="text-2xl">Senha:</h2>
+                    <input type="password" className="bg-white rounded px-3 py-2" /><br /><br />
 
-            <button onClick={() => navigate}>Cadastrar</button>
+                    <h2 className="text-2xl">Confirme sua senha:</h2>
+                    <input type="password" className="bg-white rounded px-3 py-2" /><br />
+
+                    <br />
+
+                    <button onClick={() => navigate} className="rounded px-6 py-2 bg-amber-500 self-center">Cadastrar</button>
+                </div>
+            </div>
+
         </div>
     )
 }
--- site_mass/src/pages/Login.jsx
+++ site_mass/src/pages/Login.jsx
@@ -4,17 +4,28 @@ export default function login() {
 
     const navigate = useNavigate();
 
+    function logar(){
+
+        navigate("/produtos");
+
+    }
+
     return (
-        <div className="">
-            <h2>Email:</h2>
-            <input type="text" /><br />
+        <div className="flex bg-gray-200 w-screen h-screen items-center justify-center">
+            <div className="flex w-2/5 h-120 rounded-lg bg-gray-300 p-10 items-center justify-center">
+                <div className="">
+                    <h2 className="text-2xl">Email:</h2>
+                    <input type="text" className="bg-white rounded px-3 py-2" /><br /><br />
 
-            <h2>Senha:</h2>
-            <input type="password" /><br />
+                    <h2 className="text-2xl">Senha:</h2>
+                    <input type="password" className="bg-white rounded px-3 py-2" /><br />
 
-            <p>Não tem uma conta? Cadastre-se <button onClick={() => navigate("/cadastro")}>aqui!</button></p>
+                    <p className="text-sm mt-2">Não tem uma conta? Cadastre-se <button className="underline cursor-pointer" onClick={() => navigate("/cadastro")}>aqui!</button></p><br />
 
-            <button onClick={() => ""}>Entrar</button>
+                    <button onClick={() => logar()} className="rounded px-6 py-2 bg-amber-500 self-center cursor-pointer">Entrar</button>
+                </div>
+            </div>
+
         </div>
     )
 }
