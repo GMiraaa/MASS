61de96787cd1f8e0cb1ebbb51cf677e609ef265d
GMiraaa <guifaria.mira@gmail.com>
2026-06-12 20:57:05 -0300

    OK: Add: Adicionado novas rotas

---
+++ site_mass/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+export default function Dashboard (){
+
+    return(
+        <div>
+
+        </div>
+    )
+
+}
--- site_mass/src/pages/Home.jsx
+++ site_mass/src/pages/Home.jsx
@@ -2,13 +2,13 @@ import { useNavigate } from "react-router-dom"
 import { useLocation } from "react-router-dom"
 import Sidebar from "../components/Sidebar"
 
-export default function Produtos() {
+export default function Home() {
     return (
         <main className="flex w-screen h-screen">
             <Sidebar></Sidebar>
 
             <div>
-                <p>Teste produtos</p>
+                <p>Teste home</p>
             </div>
         </main>
     )
