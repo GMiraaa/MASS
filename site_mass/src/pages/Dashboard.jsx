cde47ca56b4cf8e62f5e37b46deb9adeb2cd2efd
GMiraaa <guifaria.mira@gmail.com>
2026-06-12 21:13:00 -0300

    OK: Fix: Alterado design basico da site bar || Adicionado novas rotas e telas

--- site_mass/src/pages/Dashboard.jsx
+++ site_mass/src/pages/Dashboard.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import Sidebar from "../components/sidebar";
 
-export default function Dashboard (){
+export default function Dashboard() {
 
-    return(
-        <div>
-
-        </div>
+    return (
+        <main className="flex w-screen h-screen">
+                    <Sidebar></Sidebar>
+        
+                    <div>
+                        <p>Teste dashboard</p>
+                    </div>
+                </main>
     )
 
 }
